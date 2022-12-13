bdc2fa1a797012ea11f1ced908e083e3c9e8f7f2
pummydevi <89304803+pummydevi@users.noreply.github.com>
2022-12-13 21:11:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,61 +1,76 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
 
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+var cloud, cloudsGroup, cloudImage;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
 
+var score;
+var newImage;
 
-var world,engine;
-var ball;
-
-function setup() {
-  createCanvas(400,400);
-
-  engine = Engine.create();
-  world = engine.world;
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
   
-   var ball_options = {
-    restitution: 0.95,
-    frictionAir:0.01
-  }
-   
-   var ground_options ={
-     isStatic: true
-   };
+  groundImage = loadImage("ground2.png");
   
-  
-//create a ground
-ground = Bodies.rectangle(10,400,1000,50,ground_options);
-//add to world
-World.add(world,ground);
-
-  ball = Bodies.circle(100,10,20,ball_options);
-  World.add(world,ball);
-  
-  
-  
-
-  rectMode(CENTER);
-  ellipseMode(RADIUS);
+  cloudImage = loadImage("cloud.png");
+ // trex_running = loadAnimation("peterpan.jpg")
 }
 
+function setup() {
+  createCanvas(600, 400);
 
-function draw() 
-{
-  background(51);
-  Engine.update(engine);
-  fill("yellow")
+  trex = createSprite(50,360,20,50);
+  trex.addAnimation("running", trex_running);
+  // trex.addAnimation("collided",trex_collided)
+  trex.scale = 0.5;
+  
+  ground = createSprite(200,380,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
+  
+  invisibleGround = createSprite(200,390,400,10);
+  invisibleGround.visible = false;
   
   
-  
+}
 
-  ellipse(ball.position.x,ball.position.y,20);
-  //write a rectangle function to display ground.
-  rect(ground.position.x,ground.position.y,1000,40);
+function draw() {
+  background(180);
   
+  score = score + Math.round(getFrameRate()/60);
+  
+  
+  if(keyDown("space") && trex.y>100) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  trex.collide(invisibleGround);
+  
+  //spawn the clouds
+  spawnClouds();
+  
+  drawSprites();
+}
 
-
-  
-  
+function spawnClouds() {
+  //write code here to spawn the clouds
+  if (frameCount % 60 === 0) {
+    cloud = createSprite(600,100,40,10);
+    cloud.addImage(cloudImage)
+    cloud.y = Math.round(random(280,320))
+    
+    cloud.velocityX = -3;
+    
+    //adjust the depth
+    
+    }
 }
 
